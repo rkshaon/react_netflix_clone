3e536087b2a1808e9da86626f5b2596528ae06ba
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2021-01-17 14:45:23 +0600

    fetch movie data and movie row

--- src/App.js
+++ src/App.js
@@ -7,8 +7,18 @@ function App() {
   return (
     <div className="App">
       <h1>Building Netflix Clone using ReactJS</h1>
-      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romantic Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,23 +1,40 @@
 // component
 import React, { useState, useEffect } from 'react';
 import instance from './axios';
+import "./Row.css";
 
-function Row({ title, fetchURL }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     // this is going to be run only once, if I add depency array, then it will change everytime the value effects
     async function fetchData() {
       const request = await instance.get(fetchURL); // the url https://api.themoviedb.org/3 + fetchURL
-      console.log(request.data.results);
+      // console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-  }, []);
+  }, [fetchURL]);
+
+  console.log(movies);
 
   return(
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-      {/* container -> posters */}
+      <div className="row__posters">
+        {/* row posters */}
+        {movies.map((movie) => {
+          return <img
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              key={movie.id}
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+        })}
+      </div>
     </div>
   )
 }
